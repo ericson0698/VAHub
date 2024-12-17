f191c59daac1481ea98b4631a8bb0e0542ceaa0b
ericson0698 <44884944+ericson0698@users.noreply.github.com>
2024-12-17 13:25:19 +0800

    Delete settings.js

--- settings.js
+++
@@ -1,8 +0,0 @@
-// Get the elements
-const settingsIcon = document.getElementById('settingsIcon');
-const settingsPanel = document.getElementById('settingsPanel');
-
-// Toggle the settings panel on icon click
-settingsIcon.addEventListener('click', () => {
-    settingsPanel.classList.toggle('open');
-});
